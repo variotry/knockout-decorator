f5d5b912f53a2187bc876237effdfe15b413b17c
vario <variotries@gmail.com>
2016-06-20 17:30:25 +0900

    Implement observable decorator

--- src/knockout-decorator.ts
+++ src/knockout-decorator.ts
@@ -1,3 +1,26 @@
-﻿namespace variotry
+﻿/*!
+ * Knockout decorator
+ * (c) vario
+ * License: MIT (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+namespace variotry.KnockoutDecorator
 {
+	var storeObservableKey = "__vtKnockoutObservables__";
+	export function observable( target:any, propertyKey:string )
+	{
+		var v = target[propertyKey];
+		var o = ko.observable( v );
+		Object.defineProperty( target, propertyKey, {
+			get: () => o(),
+			set: ( v ) => o( v )
+		});
+		var d = Object.getOwnPropertyDescriptor( target, propertyKey );
+	}
+	export function observableArray()
+	{
+	}
+	export function computed()
+	{
+	}
 }
