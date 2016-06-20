ced5af517c203628dfd715f7d51d28baeb37f8ce
vario <variotries@gmail.com>
2016-06-20 19:34:46 +0900

    Add demo files.

--- demo/ts/demo.ts
+++ demo/ts/demo.ts
@@ -1,13 +1,42 @@
-﻿namespace variotry
+﻿/// <reference path="../../src/knockout-decorator.ts" />
+
+var vt = variotry.KnockoutDecorator;
+
+class Demo
 {
-	class Demo
+	@vt.observable
+	public firstName: string = "";
+
+	@vt.observable
+	private familyName: string = "";
+
+	@vt.computed()
+	private get fullName() { return this.firstName + " " + this.familyName; }
+	private set fullName( name : string )
 	{
-		private x: number = 10;
-		public constructor()
+		var items = name.split( " ", 2 );
+		this.firstName = items[0];
+		console.log( items.length );
+		if ( items.length > 1 )
 		{
-			var node = document.getElementById( "content" );
-			ko.applyBindings( this, node );
+			this.familyName = items[1];
+		}
+		else
+		{
+			this.familyName = "";
 		}
 	}
-	new Demo();
+
+	public constructor()
+	{
+		var node = document.getElementById( "content" );
+		ko.applyBindings( this, node );
+
+		// test get observable object from property name.
+		vt.getObservable<string>( this, "firstName" ).subscribe( newValue =>
+		{
+			console.log( "new value is", newValue );
+		});
+	}
 }
+new Demo();
